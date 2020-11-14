a81f097200f0683d7cbd7a386e924745eafcda76
Max <mx999966@gmail.com>
2020-11-14 09:15:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,5 +20,11 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch(age){
+    case "13":
+      return "You are a teenager"
+    default:
+      return "You have an age"
+    
+  }
 }
