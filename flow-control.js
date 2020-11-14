0231b79ba43a1cc8a636b6a7c30d9ecf42c9cd56
Max <mx999966@gmail.com>
2020-11-14 09:08:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  (age < 13 && age > 19) ? "You are not a teenager" : "You are a teenager"
+  return (age < 13 && age > 19) ? "You are not a teenager" : "You are a teenager"
 }
 
 function switchAge(age) {
